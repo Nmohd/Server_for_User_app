6fce0f65b0b0c51b082f3aa1d9aec78bb8eb94f9
Nmohd <najeebu50@gmail.com>
2023-12-07 19:17:08 +0530

    okay

--- connection.js
+++ connection.js
@@ -1,17 +1,6 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   //   .connect("mongodb+srv://unknown:12345@sandbox.1nsjpu6.mongodb.net/user_app")
-//   .then(() => {
-//     console.log("DB Connected....");
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//     console.log("Something went wrong......");
-//   });
-
 dotenv.config();
 // console.log(process.env.MONGO_URI);
 
