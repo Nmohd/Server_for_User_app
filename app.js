f10b5a58c2920dce18508b170a71567fff0b4909
Nmohd <najeebu50@gmail.com>
2023-12-07 12:33:37 +0530

    cors added

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require("express");
 const app = express();
 const connectDB = require("./connection");
 
-
 connectDB();
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const PORT = process.env.PORT || 2080;
 console.log(PORT);
